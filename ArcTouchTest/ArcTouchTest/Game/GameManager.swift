91f8e2feca8372de9bf0ab4d83f62aefa5a6fb38
Rafael <41241789@mackenzista.com.br>
2020-02-08 18:15:24 -0300

    Updated GameManagerProtocol and GameManager class (no business logic though)

--- ArcTouchTest/ArcTouchTest/Game/GameManager.swift
+++ ArcTouchTest/ArcTouchTest/Game/GameManager.swift
@@ -10,10 +10,51 @@ import Foundation
 
 protocol GameManagerProtocol {
     var game: Game { get set }
+    var timer: Timer! { get set }
+    var timeInSeconds: Int! {get}
+    var timeElapsed: Int! {get set}
     func checkKeyword(with inputValue: String)
     func startGame()
-    func endGame()
-    func prepareGame()
-    func isGameRunning()
-    func isGameStopped()
+    func endGame(completionHandler: @escaping () -> ())
+    func isGameRunning() -> Bool
+    func performUpdates()
+    func timeLeft() -> Int
+}
+
+class GameManager: GameManagerProtocol {
+    
+    var game: Game
+    var timer: Timer!
+    var timeInSeconds: Int!
+    var timeElapsed: Int!
+    init(with game: Game, and timeInSeconds: Int) {
+        self.game = game
+        self.timer = Timer(timeInterval: 1, target: self, selector: #selector(performUpdates), userInfo: nil, repeats: false)
+        self.timeInSeconds = timeInSeconds
+        timeElapsed = 0
+    }
+    
+    func checkKeyword(with inputValue: String) {
+        
+    }
+    
+    func startGame() {
+        
+    }
+    
+    func endGame(completionHandler: @escaping () -> ()) {
+        
+    }
+        
+    func isGameRunning() -> Bool {
+        fatalError("Not Implemented yet")
+    }
+    
+    @objc func performUpdates() {
+        
+    }
+    
+    func timeLeft() -> Int {
+        fatalError("Not Implemented yet")
+    }
 }
---
+++ ArcTouchTest/ArcTouchTestTests/GameManagerTests.swift
@@ -0,0 +1,25 @@
+//
+//  GameManagerTests.swift
+//  ArcTouchTestTests
+//
+//  Created by Rafael  Hieda on 2/8/20.
+//  Copyright © 2020 Rafael_Hieda. All rights reserved.
+//
+
+import XCTest
+@testable import ArcTouchTest
+
+class GameManagerTests: XCTestCase {
+    
+    let sut: GameManagerProtocol!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = GameManager
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
